f551a350d0712b51cc43d6e5fe4e4ff78e5b8ede
linpeng.ding <linpeng.ding@yuelian-inc.com>
2014-09-18 19:15:13 +0800

    modified the version and author info

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# author: xilin.ding
+# author: xiLin.ding
 
 
 from setuptools import setup, find_packages
@@ -8,18 +8,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_render',
-    version='1.0.1',
+    version='0.9.0',
     packages=find_packages(),
-    author='GaussDing',
-    author_email='DDYDLP@gmail.com',
-    url='https://github.com/GaussDing',
+    author='WANG Tai',
+    author_email='i@wangtai.me',
+    url='https://github.com/wangtai',
     description='a very light django plugin',
-	long_description=open('README.md').read(),
+    long_description=open('README.md').read(),
     license='Apache2',
-	requires=[
+    requires=[
         'Django'
     ],
-	classifiers=[
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -29,6 +29,6 @@ setup(
         'Programming Language :: Python',
         'Topic :: System :: Installation/Setup'
     ],
-	include_package_data=True,
+    include_package_data=True,
     zip_safe=False
 )
